64c79b8cb1ee5d82f857b0bcf9f55dd526f9c6a3
Lou Oberto <louoberto@gmail.com>
2025-01-10 19:14:06 -0700

    Bug in star spacing

--- source/star_spacing.py
+++ source/star_spacing.py
@@ -6,7 +6,7 @@
 # ========================================================================
 def star_spacing(self, j, char, code_line, temp_line):
     if code_line[j - 1] != self.space and code_line[j + 1] != self.space: #?*?
-        if code_line[j - 1].isalnum() and code_line[j + 1].isalnum(): #5*5
+        if code_line[j - 1].isalnum() and (code_line[j + 1].isalnum() or code_line[j + 1] == '('): #5*5
             temp = temp_line + self.space + char + self.space
         elif code_line[j - 1].isalnum() and not code_line[j + 1].isalnum(): #5*?
             temp = temp_line + self.space + char
