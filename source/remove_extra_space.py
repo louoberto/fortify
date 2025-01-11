18ec8fb5fc9f362a1eb72865d0530667e2a9afbc
Lou Oberto <louoberto@gmail.com>
2025-01-11 12:41:40 -0700

    Code cleanup in remove_extra_spaces

--- source/remove_extra_space.py
+++ source/remove_extra_space.py
@@ -5,23 +5,12 @@
 # Removes double spacing in a code line.
 # ========================================================================
 def remove_extra_space(self, j, char, code_line, temp_line):
-    if code_line[j + 1] in [self.newline, self.space]:
+    if code_line[j + 1] in [self.newline, self.space] or \
+       code_line[j - 1] == "(" or \
+       code_line[j + 1] == ")" or \
+       temp_line[-1] == "(":
         temp = temp_line
     else:
-        # print(repr(temp_line), repr(code_line[j - 1]), repr(code_line[j + 1]))
-        if code_line[j - 1] == "(": # Remove blank space after paren
-            # print(repr(temp_line))
-            temp = temp_line
-        else:
-            if code_line[j + 1] == ")":
-                temp = temp_line
-            elif code_line[j - 1] == "(":
-                temp = temp_line
-                # print(repr(temp))
-            elif temp_line[-1] == "(":
-                temp = temp_line
-            else:
-                temp = temp_line + char
-                # print(repr(temp))
+        temp = temp_line + char
 
     return temp
