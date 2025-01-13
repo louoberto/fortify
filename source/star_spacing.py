293b33265a90bb07034c659e31bb94da77a793cb
Lou Oberto <louoberto@gmail.com>
2025-01-13 01:37:30 -0700

    Spacing for characters

--- source/star_spacing.py
+++ source/star_spacing.py
@@ -7,7 +7,12 @@
 def star_spacing(self, j, char, code_line, temp_line):
     if code_line[j - 1] != self.space and code_line[j + 1] != self.space: #?*?
         if code_line[j - 1].isalnum() and (code_line[j + 1].isalnum() or code_line[j + 1] == '('): #5*5
-            temp = temp_line + self.space + char + self.space
+            for type in self.data_types:
+                if code_line.lower().startswith(type) and len(type) == j:
+                    temp = temp_line + char
+                    break
+                else:
+                    temp = temp_line + self.space + char + self.space
         elif code_line[j - 1].isalnum() and not code_line[j + 1].isalnum(): #5*?
             temp = temp_line + self.space + char
         elif not code_line[j - 1].isalnum() and code_line[j + 1].isalnum(): #?*5
