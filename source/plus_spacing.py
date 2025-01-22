07bc3753486c395fee2b40963aff2246fca0dbac
Lou Oberto <louoberto@gmail.com>
2025-01-21 21:18:33 -0700

    spacing

--- source/plus_spacing.py
+++ source/plus_spacing.py
@@ -34,7 +34,7 @@ def plus_spacing(self, j, char, code_line, temp_line):
         if code_line[j - 2] in ['=', ','] and (code_line[j + 1].isalnum() or code_line[j + 1] == '('):
             temp = temp_line + char
         else:
-            if code_line[j - 1] == self.space and code_line[j + 1].isalnum():
+            if code_line[j - 1] == self.space and (code_line[j + 1].isalnum() or code_line[j + 1] == self.newline):
                 temp = temp_line + char
             else:
                 temp = temp_line + char + self.space
