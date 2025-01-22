8a08f5591768fc5a373364a327a971075f0d81c1
Lou Oberto <louoberto@gmail.com>
2025-01-21 21:47:38 -0700

    fixed_form

--- source/comma_spacing.py
+++ source/comma_spacing.py
@@ -9,6 +9,8 @@ def comma_spacing(self, j, char, code_line, temp_line):
         temp = temp_line[:-1] + char
     elif len(code_line) > j + 1 and code_line[j + 1] not in [self.space, self.continuation_char, self.newline]: # Comma case 2
         temp = temp_line + char + self.space
+    elif not self.free_form and len(code_line) > j + 1 and code_line[j + 1] not in [self.space, self.newline]:
+        temp = temp_line + char + self.space
     else:
         temp = temp_line + char
     return temp
