6c4517b03c3e432484abc95085bb0df7fbab6ee4
Lou Oberto <louoberto@gmail.com>
2025-04-01 17:40:18 -0700

    doublecolon

--- source/colon_spacing.py
+++ source/colon_spacing.py
@@ -14,7 +14,11 @@ def colon_spacing(self, j, char, code_line, temp_line):
     if re.search(r"^[a-z0-9_]+:*" + re.escape('do ') + r"\b", code_line.lower()):
         if debug_me:
             self.debug(currentframe().f_lineno, char, code_line, j)
-        return temp_line + char + self.space 
+        return temp_line + char + self.space
+    elif len(code_line) > j + 1 and code_line[j + 1] == ':' and code_line[j - 1] not in [self.space,')']:
+        return temp_line + self.space + char
+    elif len(code_line) > j + 1 and code_line[j + 1] not in [self.space] and code_line[j - 1] == ':':
+        return temp_line + char + self.space
     else:
         # print(code_line, repr(code_line[j]),j)
         return temp_line + char
